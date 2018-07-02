4ec6eff8015a99dc6075d5ccaea1533b855c7966
Loic Teixeira <loic@springload.co.nz>
2018-07-02 15:41:46 +1000

    Make edit handlers Wagtail 2.0 compatible

--- wagtailmodelchoosers/edit_handlers.py
+++ wagtailmodelchoosers/edit_handlers.py
@@ -5,37 +5,9 @@ from wagtailmodelchoosers.utils import flatten, get_chooser_options
 from wagtailmodelchoosers.widgets import ModelChooserWidget, RemoteModelChooserWidget
 
 
-class BaseModelChooserPanel(BaseChooserPanel):
+class ModelChooserPanel(BaseChooserPanel):
     object_type_name = 'model'
 
-    @cached_classmethod
-    def target_model(cls):
-        return cls.model._meta.get_field(cls.field_name).rel.to
-
-    @classmethod
-    def get_required(cls):
-        null = cls.model._meta.get_field(cls.field_name).null
-        return not null
-
-    @classmethod
-    def widget_overrides(cls):
-        return {
-            cls.field_name: ModelChooserWidget(
-                cls.target_model(),
-                required=cls.get_required(),
-                label=cls.label,
-                display=cls.display,
-                list_display=cls.list_display,
-                filters=cls.filters,
-                page_size_param=cls.page_size_param,
-                page_size=cls.page_size,
-                pk_name=cls.pk_name,
-                translations=cls.translations,
-            )
-        }
-
-
-class ModelChooserPanel(object):
     def __init__(self, field_name, chooser, **kwargs):
         options = get_chooser_options(chooser)
         options.update(kwargs)
@@ -51,49 +23,49 @@ class ModelChooserPanel(object):
         self.pk_name = options.pop('pk_name', 'uuid')
         self.translations = options.pop('translations', [])
 
-    def bind_to_model(self, model):
-        return type(str('_ModelChooserPanel'), (BaseModelChooserPanel,), {
-            'model': model,
-            'field_name': self.field_name,
-            'label': self.label,
-            'display': self.display,
-            'list_display': self.list_display,
-            'filters': self.filters,
-            'page_size_param': self.page_size_param,
-            'page_size': self.page_size,
-            'pk_name': self.pk_name,
-            'translations': self.translations,
-        })
+        # The `ModelChooserBlock` required the `content_type` option
+        # which the pannel does not need, poping it out.
+        options.pop('content_type', None)
 
+        super().__init__(field_name, **options)
 
-class BaseRemoteModelChooserPanel(BaseChooserPanel):
-    object_type_name = 'remote_model'
+    def target_model(self):
+        return self.model._meta.get_field(self.field_name).related_model
 
-    @classmethod
-    def get_required(cls):
-        blank = cls.model._meta.get_field(cls.field_name).blank
-        return not blank
+    def get_required(self):
+        null = self.model._meta.get_field(self.field_name).null
+        return not null
 
-    @classmethod
-    def widget_overrides(cls):
+    def widget_overrides(self):
         return {
-            cls.field_name: RemoteModelChooserWidget(
-                cls.chooser,
-                required=cls.get_required(),
-                label=cls.label,
-                display=cls.display,
-                list_display=cls.list_display,
-                filters=cls.filters,
-                page_size_param=cls.page_size_param,
-                page_size=cls.page_size,
-                fields_to_save=cls.fields_to_save,
-                pk_name=cls.pk_name,
-                translations=cls.translations,
+            self.field_name: ModelChooserWidget(
+                self.target_model(),
+                required=self.get_required(),
+                label=self.label,
+                display=self.display,
+                list_display=self.list_display,
+                filters=self.filters,
+                page_size_param=self.page_size_param,
+                page_size=self.page_size,
+                pk_name=self.pk_name,
+                translations=self.translations,
             )
         }
 
+    def clone(self):
+        return self.__class__(
+            field_name=self.field_name,
+            widget=self.widget if hasattr(self, 'widget') else None,
+            heading=self.heading,
+            classname=self.classname,
+            help_text=self.help_text,
+            chooser=self.chooser,
+        )
 
-class RemoteModelChooserPanel(object):
+
+class RemoteModelChooserPanel(BaseChooserPanel):
+    object_type_name = 'remote_model'
+
     def __init__(self, field_name, chooser, **kwargs):
         options = get_chooser_options(chooser)
         options.update(kwargs)
@@ -110,18 +82,25 @@ class RemoteModelChooserPanel(object):
         self.pk_name = options.pop('pk_name', 'uuid')
         self.translations = options.pop('translations', [])
 
-    def bind_to_model(self, model):
-        return type(str('_RemoteModelChooserPanel'), (BaseRemoteModelChooserPanel,), {
-            'model': model,
-            'field_name': self.field_name,
-            'chooser': self.chooser,
-            'label': self.label,
-            'display': self.display,
-            'list_display': self.list_display,
-            'filters': self.filters,
-            'page_size_param': self.page_size_param,
-            'page_size': self.page_size,
-            'fields_to_save': self.fields_to_save,
-            'pk_name': self.pk_name,
-            'translations': self.translations,
-        })
+        super().__init__(field_name, **options)
+
+    def get_required(self):
+        blank = self.model._meta.get_field(self.field_name).blank
+        return not blank
+
+    def widget_overrides(self):
+        return {
+            self.field_name: RemoteModelChooserWidget(
+                self.chooser,
+                required=self.get_required(),
+                label=self.label,
+                display=self.display,
+                list_display=self.list_display,
+                filters=self.filters,
+                page_size_param=self.page_size_param,
+                page_size=self.page_size,
+                fields_to_save=self.fields_to_save,
+                pk_name=self.pk_name,
+                translations=self.translations,
+            )
+        }
