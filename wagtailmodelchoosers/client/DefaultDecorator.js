46fcb4bff2f21bb11746f4ae5b2ffd013700b581
aaronhaslett <aaron.haslett@springload.co.n>
2021-05-24 11:43:06 +1200

    Switching from wagtaildraftail to upstream wagtail's draftail

---
+++ wagtailmodelchoosers/client/DefaultDecorator.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const DefaultDecorator = (entityData, props) => {
+  const { entityKey, contentState, children } = props;
+  const d = contentState.getEntity(entityKey).getData();
+  const { id } = d;
+
+  const prefixLabel = entityData.prefix_label || '';
+  const prefix = `#${id} ${prefixLabel} - `;
+
+  return (
+      <span data-tooltip={entityKey} className="RichEditor-link">
+          {entityData.icon}
+          {prefix}
+          {children}
+      </span>
+  );
+};
+
+DefaultDecorator.propTypes = {
+    entityKey: PropTypes.string.isRequired,
+    contentState: PropTypes.object.isRequired,
+    children: PropTypes.node.isRequired,
+};
+
+export default DefaultDecorator;
--- wagtailmodelchoosers/client/sources/ModelSource.js
+++ wagtailmodelchoosers/client/sources/ModelSource.js
@@ -1,87 +1,95 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { DraftUtils } from 'draftail';
-
+import { Modifier, EditorState } from 'draft-js';
 import ModelPicker from '../components/ModelPicker';
 
-const API_BASE_URL = '/admin/modelchoosers/api/v1/model/';
+const API_BASE_URL = '/admin/modelchoosers/api/v1';
+
 
 class ModelSource extends React.Component {
   constructor(props) {
     super(props);
-    this.onClose = this.onClose.bind(this);
     this.onSelected = this.onSelected.bind(this);
   }
 
-  onClose() {
-    const { onClose } = this.props;
-    onClose();
-  }
-
   onSelected(id, data) {
-    const { editorState, onUpdate, options: { content_type, display = 'title', pk_name: pkName = 'uuid', type } } = this.props;
+    const {
+      editorState,
+      onComplete,
+      entityType: {
+        content_type,
+        display = 'title',
+        pk_name: pkName = 'uuid',
+        type,
+        icon,
+        icon_name,
+        fields_to_save: fieldsToSave = [],
+      }
+    } = this.props;
+
     let label;
-    let entityMutability;
-    if (display === '__selection__') {
-      const selectionState = editorState.getSelection();
+    const displayIsSelection = display === '__selection__';
+
+    const currentContent = editorState.getCurrentContent();
+    const selectionState = editorState.getSelection();
+
+    if (displayIsSelection) {
+      // If display is selection, get label for the new entity from selected text
+      // TODO: What if no text is selected?
       const anchorKey = selectionState.getAnchorKey();
-      const currentContent = editorState.getCurrentContent();
       const currentContentBlock = currentContent.getBlockForKey(anchorKey);
+
       const start = selectionState.getStartOffset();
       const end = selectionState.getEndOffset();
 
       label = currentContentBlock.getText().slice(start, end);
-      entityMutability = 'MUTABLE';
     } else {
-      if (Array.isArray(display)) {
-        let i;
-        for (i = 0; i < display.length; i + 1) {
-          const fieldName = display[i];
-          if (fieldName in data && data[fieldName]) {
-            label = data[fieldName];
-            break;
-          }
-        }
-      } else if (display in data && data[display]) {
-        label = data[display];
-      }
-
-      if (label === undefined) {
-        label = data[pkName];
-      }
-      entityMutability = 'IMMUTABLE';
+      // Otherwise, assume 'display' is an array of fields to lookup in the selected
+      // data for a label name.  If none match, use pkName as a backup.
+      const display_arr = Array.isArray(display) ? display : [display];
+      const field = display_arr.find((f) => f in data && data[f]) || pkName;
+      label = data[field];
     }
+
+    const fields = Object.fromEntries(fieldsToSave.map((f) => [f, data[f]]));
+
+    // For some reason we have to only store content_type for local models.
+    // I have no idea why, just preserving existing functionality.
+    const maybeContentType = fieldsToSave.length === 0 ? {content_type} : {};
+
     const nextData = {
       id,
       label,
-      content_type,
+      ...fields,
+      ...maybeContentType,
     };
 
-    const nextState = DraftUtils.createEntity(
-      editorState,
-      type,
-      nextData,
-      nextData.label,
-      entityMutability,
-    );
+    const entityMutability = displayIsSelection ? 'MUTABLE' : 'IMMUTABLE';
+    const entity = currentContent.createEntity(type, entityMutability, nextData);
+    const entityKey = entity.getLastCreatedEntityKey();
 
-    onUpdate(nextState);
+    const newContent = Modifier.replaceText(currentContent, selectionState, label, null, entityKey);
+    const nextState = EditorState.push(editorState, newContent, 'insert-characters');
+
+    onComplete(nextState);
   }
 
   render() {
-    const { entity, options } = this.props;
-    const endpoint = `${API_BASE_URL}${options.content_type}`;
+    const { entity, entityType } = this.props;
+    const { fields_to_save = null } = entityType;
+    const model_path = fields_to_save ? 'remote_model' : 'model'
+    const endpoint = `${API_BASE_URL}/${model_path}/${entityType.content_type}`;
 
     return (
       <ModelPicker
         onChange={this.onSelected}
         onSelect={this.onSelected}
-        onClose={this.onClose}
+        onClose={this.props.onClose}
         entity={entity}
         endpoint={endpoint}
         value={null}
         required={false}
-        {...options}
+        {...entityType}
       />
     );
   }
