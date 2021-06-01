d6cc6e198d5650f9e3fe341676252adb4e862f4a
Liam Johnston <liamjohnston.nz@gmail.com>
2021-06-01 14:27:10 +1200

    Add typescript and ts-ify new files

---
+++ wagtailmodelchoosers/client/DefaultDecorator.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type Props = {
+    entityKey: string;
+    contentState: any;
+    children: React.ReactNode;
+};
+
+const DefaultDecorator = (entityData: any, props: Props) => {
+    console.log("DefaultDecorator entityData: ", entityData);
+    console.log("DefaultDecorator props: ", props);
+
+    const { entityKey, contentState, children } = props;
+    const d = contentState.getEntity(entityKey).getData();
+    const { id } = d;
+
+    const prefixLabel = entityData.prefix_label || "";
+    const prefix = `#${id} ${prefixLabel} - `;
+
+    return (
+        <span data-tooltip={entityKey} className="RichEditor-link">
+            {prefix}
+            {children}
+        </span>
+    );
+};
+
+export default DefaultDecorator;
--- wagtailmodelchoosers/client/webpack.config.dev.js
+++ wagtailmodelchoosers/client/webpack.config.dev.js
@@ -1,33 +1,40 @@
-const path = require('path');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const outputPath = path.join(__dirname, '..', 'static', 'wagtailmodelchoosers');
+const outputPath = path.join(__dirname, "..", "static", "wagtailmodelchoosers");
 
 module.exports = {
   entry: {
-    wagtailmodelchoosers: './wagtailmodelchoosers/client/wagtailmodelchoosers.js',
-    draftailmodelchoosers: './wagtailmodelchoosers/client/draftailmodelchoosers.js',
-    polyfills: './wagtailmodelchoosers/client/polyfills.js',
+    wagtailmodelchoosers:
+      "./wagtailmodelchoosers/client/wagtailmodelchoosers.tsx",
+    draftailmodelchoosers:
+      "./wagtailmodelchoosers/client/draftailmodelchoosers.js",
+    polyfills: "./wagtailmodelchoosers/client/polyfills.js",
   },
   output: {
     path: outputPath,
-    filename: '[name].js',
+    filename: "[name].js",
   },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new MiniCssExtractPlugin(),
-  ],
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
+  plugins: [new webpack.NoEmitOnErrorsPlugin(), new MiniCssExtractPlugin()],
   module: {
     rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
       {
         test: /\.js$/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
