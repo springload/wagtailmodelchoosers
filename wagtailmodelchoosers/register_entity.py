4dee9e3e4080a465a3502b762f1421f0d015e418
aaronhaslett <aaron.haslett@springload.co.n>
2021-12-06 15:54:28 +1300

    Refactor and improve entity registration

---
+++ wagtailmodelchoosers/register_entity.py
@@ -0,0 +1,92 @@
+from django.conf import settings
+from django.utils.module_loading import import_string
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+from draftjs_exporter.dom import DOM
+from wagtail.core.rich_text import LinkHandler
+from wagtail.admin.rich_text.converters.html_to_contentstate import InlineEntityElementHandler
+
+
+"""
+This basically revives the old way of registering draftail extensions where you don't have to
+think about from_database or to_database matching rules.  Also it wraps old style decorators
+into a link rewriter in a hacky way.
+"""
+
+ID_ATTR_NAME = "modelchooser_id"
+
+
+class FromDBHandler(InlineEntityElementHandler):
+    mutability = "IMMUTABLE"
+
+    def get_attribute_data(self, attrs):
+        self.attrs = attrs
+        extra = {}
+        if ID_ATTR_NAME in attrs:
+            extra = {"id": attrs[ID_ATTR_NAME]}
+        return dict(attrs, **extra)
+
+    # This is a hack that allows us to store <a> tags without children
+    # If it has broken due to upgrade, you have to refactor how modelchoosers
+    # stores stuff and possibly do a data migration.  My prayers are with you.
+    def handle_endtag(self, name, state, contentstate):
+        label = self.attrs.get("label", None)
+        if label:
+            state.current_block.text = label
+        return super().handle_endtag(name, state, contentstate)
+
+
+def default_expand_attrs(attrs):
+    href = attrs.pop("url")
+    attrs.pop("linktype")
+    tag = DOM.create_element("a", dict(attrs, **{"href": href}))
+    return str(DOM.render(tag))
+
+
+def make_rewriter(t, expander):
+    class ModelLinkHandler(LinkHandler):
+        identifier = t
+
+        @classmethod
+        def expand_db_attributes(cls, attrs):
+            return str(DOM.render(expander(attrs)))
+
+    return ModelLinkHandler
+
+
+def to_database_format(t):
+    def inner(props):
+        children = props.pop("children")
+
+        props["linktype"] = t
+        if "id" in props:
+            props[ID_ATTR_NAME] = str(props["id"])
+
+        if "label" in props:
+            children = []
+
+        return DOM.create_element("a", props, children)
+    return inner
+
+
+def register_entity(features, chooser, name, draftail_type, prefix):
+        feature = draftail_features.EntityFeature(chooser)
+        features.register_editor_plugin("draftail", name, feature)
+
+        # This has to be lower because html attr names get lowered and
+        # the DOM selector has to match.  The type in the actual handler
+        # doesn't get lowered because it's the real type draftail is expecting.
+        t = prefix + draftail_type.lower()
+
+        rule = {
+            "from_database_format": {"a[linktype={}]".format(t): FromDBHandler(draftail_type)},
+            "to_database_format": {"entity_decorators": {draftail_type: to_database_format(t)}},
+        }
+
+        features.register_converter_rule("contentstate", name, rule)
+
+        expander_path = getattr(settings, "DRAFT_EXPORTER_ENTITY_DECORATORS", {}).get(draftail_type, None)
+        expander = default_expand_attrs
+        if expander_path:
+            expander = import_string(expander_path)
+
+        features.register_link_type(make_rewriter(t, expander))
--- wagtailmodelchoosers/wagtail_hooks.py
+++ wagtailmodelchoosers/wagtail_hooks.py
@@ -1,13 +1,8 @@
-import wagtail.admin.rich_text.editors.draftail.features as draftail_features
-from django.conf import settings
 from django.urls import re_path
 from django.templatetags.static import static
 from django.utils.html import format_html, format_html_join
-from django.utils.module_loading import import_string
-from draftjs_exporter.dom import DOM
-from wagtail.admin.rich_text.converters.html_to_contentstate import InlineEntityElementHandler
-from wagtail.core.rich_text import LinkHandler
 from wagtail.core import hooks
+from .register_entity import register_entity
 
 from wagtailmodelchoosers.views import ModelView, RemoteResourceView
 
@@ -52,50 +47,6 @@ def wagtailmodelchoosers_admin_urls():
     ]
 
 
-class FromDBHandler(InlineEntityElementHandler):
-    mutability = "IMMUTABLE"
-
-    def get_attribute_data(self, attrs):
-        self.attrs = attrs
-        return dict(attrs, **{"id": attrs["modelchooser_id"]})
-
-    # This is a hack that allows us to store <a> tags without children
-    # If it has broken due to upgrade, you have to refactor how modelchoosers
-    # stores stuff and possibly do a data migration.  My prayers are with you.
-    def handle_endtag(self, name, state, contentstate):
-        state.current_block.text = self.attrs["label"]
-        return super().handle_endtag(name, state, contentstate)
-
-
-def default_expand_attrs(attrs):
-    href = attrs.pop("url")
-    attrs.pop("linktype")
-    tag = DOM.create_element("a", dict(attrs, **{"href": href}))
-    return str(DOM.render(tag))
-
-
-def make_rewriter(t, expander):
-    class ModelLinkHandler(LinkHandler):
-        identifier = t
-
-        @classmethod
-        def expand_db_attributes(cls, attrs):
-            return str(DOM.render(expander(attrs)))
-
-    return ModelLinkHandler
-
-
-def to_database_format(t):
-    def inner(props):
-        children = props.pop("children")
-
-        props["linktype"] = t
-        props["modelchooser_id"] = str(props["id"])
-
-        return DOM.create_element("a", props)
-    return inner
-
-
 @hooks.register("register_rich_text_features")
 def register_rich_text_features(features):
     choosers = get_all_chooser_options()
@@ -110,21 +61,4 @@ def register_rich_text_features(features):
             chooser["content_type"] = name
 
         chooser["type"] = draftail_type
-        feature = draftail_features.EntityFeature(chooser)
-        features.register_editor_plugin("draftail", name, feature)
-
-        t = "model_chooser_" + draftail_type.lower()
-
-        rule = {
-            "from_database_format": {"a[linktype={}]".format(t): FromDBHandler(draftail_type)},
-            "to_database_format": {"entity_decorators": {draftail_type: to_database_format(t)}},
-        }
-
-        features.register_converter_rule("contentstate", name, rule)
-
-        expander_path = getattr(settings, "DRAFT_EXPORTER_ENTITY_DECORATORS", {}).get(draftail_type, None)
-        expander = default_expand_attrs
-        if expander_path:
-            expander = import_string(expander_path)
-
-        features.register_link_type(make_rewriter(t, expander))
+        register_entity(features, chooser, name, draftail_type, "model_chooser_")
