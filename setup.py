da3c77cbffc0783fdc2d1893efe0569ec6728e89
Calvin Giles <calvin.giles@springload.co.nz>
2021-12-08 13:55:54 +1300

    Support wagtail 2.15, and update version identifier

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 
 install_requires = [
-    'wagtail>=2.0.2,<2.15',  # Depends on Wagtail's Django and Django RestFramework depencencies
+    'wagtail>=2.0.2,<2.16',  # Depends on Wagtail's Django and Django RestFramework depencencies
     'django-filter>=2.2.0,<2.5.0'
 ]
 
